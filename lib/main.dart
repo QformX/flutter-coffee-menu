c4afcb35c50c8908962e43fbe51cf01243947e52
QForm <mr.bogdan2003@gmail.com>
2024-06-30 16:40:03 +0600

    refactor: main

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_coffee_menu/src/app.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MenuScreen(),
     );
   }
 }
