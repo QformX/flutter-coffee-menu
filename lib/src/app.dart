b68a91387f47d18186a4e527f9b4d1e0345a0ec1
QForm <mr.bogdan2003@gmail.com>
2024-07-03 13:58:31 +0600

    feat: add separate file for constants

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter_coffee_menu/src/common/constants/constant.dart';
 import 'package:flutter_coffee_menu/src/common/widgets/category.dart';
 import 'package:flutter_coffee_menu/src/common/widgets/item.dart';
 
@@ -18,119 +19,10 @@ class _MenuScreenState extends State<MenuScreen> {
     'Авторские напитки': GlobalKey(),
     'Десерты': GlobalKey(),
   };
-  final String defaultImageUrl = 'https://cdn2.iconfinder.com/data/icons/drinks-and-beverage-containers/100/08-512.png';
-  final bool temp = true;
 
-  final Map<String, List<CoffeeItemData>> _categoryData = {
-  'Черный кофе': [
-    CoffeeItemData(
-        imageUrl: 'https://cdn2.iconfinder.com/data/icons/drinks-and-beverage-containers/100/08-512.png',
-        name: 'Эспрессо',
-        price: 100),
-    CoffeeItemData(
-        imageUrl: 'https://cdn2.iconfinder.com/data/icons/drinks-and-beverage-containers/100/08-512.png',
-        name: 'Американо',
-        price: 120),
-    CoffeeItemData(
-        imageUrl: 'https://cdn2.iconfinder.com/data/icons/drinks-and-beverage-containers/100/08-512.png',
-        name: 'Ристретто',
-        price: 130),
-    CoffeeItemData(
-        imageUrl: 'https://cdn2.iconfinder.com/data/icons/drinks-and-beverage-containers/100/08-512.png',
-        name: 'Лунго',
-        price: 140),
-    CoffeeItemData(
-        imageUrl: 'https://cdn2.iconfinder.com/data/icons/drinks-and-beverage-containers/100/08-512.png',
-        name: 'Доппио',
-        price: 150),
-    CoffeeItemData(
-        imageUrl: 'https://cdn2.iconfinder.com/data/icons/drinks-and-beverage-containers/100/08-512.png',
-        name: 'Макиато',
-        price: 160),
-  ],
-  'Кофе с молоком': [
-    CoffeeItemData(
-        imageUrl: 'https://cdn2.iconfinder.com/data/icons/drinks-and-beverage-containers/100/08-512.png',
-        name: 'Латте',
-        price: 170),
-    CoffeeItemData(
-        imageUrl: 'https://cdn2.iconfinder.com/data/icons/drinks-and-beverage-containers/100/08-512.png',
-        name: 'Капучино',
-        price: 180),
-    CoffeeItemData(
-        imageUrl: 'https://cdn2.iconfinder.com/data/icons/drinks-and-beverage-containers/100/08-512.png',
-        name: 'Флэт Уайт',
-        price: 190),
-    CoffeeItemData(
-        imageUrl: 'https://cdn2.iconfinder.com/data/icons/drinks-and-beverage-containers/100/08-512.png',
-        name: 'Мокко',
-        price: 200),
-    CoffeeItemData(
-        imageUrl: 'https://cdn2.iconfinder.com/data/icons/drinks-and-beverage-containers/100/08-512.png',
-        name: 'Бреве',
-        price: 210),
-    CoffeeItemData(
-        imageUrl: 'https://cdn2.iconfinder.com/data/icons/drinks-and-beverage-containers/100/08-512.png',
-        name: 'Рафф',
-        price: 220),
-  ],
-  'Чай': [
-    CoffeeItemData(
-        imageUrl: 'https://cdn2.iconfinder.com/data/icons/drinks-and-beverage-containers/100/08-512.png',
-        name: 'Зеленый чай',
-        price: 100),
-    CoffeeItemData(
-        imageUrl: 'https://cdn2.iconfinder.com/data/icons/drinks-and-beverage-containers/100/08-512.png',
-        name: 'Черный чай',
-        price: 120),
-    CoffeeItemData(
-        imageUrl: 'https://cdn2.iconfinder.com/data/icons/drinks-and-beverage-containers/100/08-512.png',
-        name: 'Улун',
-        price: 130),
-    CoffeeItemData(
-        imageUrl: 'https://cdn2.iconfinder.com/data/icons/drinks-and-beverage-containers/100/08-512.png',
-        name: 'Пуэр',
-        price: 140),
-    CoffeeItemData(
-        imageUrl: 'https://cdn2.iconfinder.com/data/icons/drinks-and-beverage-containers/100/08-512.png',
-        name: 'Холодный чай',
-        price: 150),
-  ],
-  'Авторские напитки': [
-    CoffeeItemData(
-        imageUrl: 'https://cdn2.iconfinder.com/data/icons/drinks-and-beverage-containers/100/08-512.png',
-        name: 'Ароматное Перо',
-        price: 200),
-    CoffeeItemData(
-        imageUrl: 'https://cdn2.iconfinder.com/data/icons/drinks-and-beverage-containers/100/08-512.png',
-        name: 'Кофе Мастерской',
-        price: 220),
-    CoffeeItemData(
-        imageUrl: 'https://cdn2.iconfinder.com/data/icons/drinks-and-beverage-containers/100/08-512.png',
-        name: 'Вампир',
-        price: 220),
-  ],
-  'Десерты': [
-    CoffeeItemData(
-        imageUrl: 'https://cdn-icons-png.flaticon.com/256/76/76257.png',
-        name: 'Чизкейк',
-        price: 300),
-    CoffeeItemData(
-        imageUrl: 'https://cdn-icons-png.flaticon.com/256/76/76257.png',
-        name: 'Тирамису',
-        price: 320),
-    CoffeeItemData(
-        imageUrl: 'https://cdn-icons-png.flaticon.com/256/76/76257.png',
-        name: 'Безе',
-        price: 340),
-    CoffeeItemData(
-        imageUrl: 'https://cdn-icons-png.flaticon.com/256/76/76257.png',
-        name: 'Панна котта',
-        price: 400),
-    ],
-  };
+  final Map<String, List<CoffeeItemData>> _categoryData = categoryData;
 
-  String _activeCategory = 'Черный кофе';
+  String _activeCategory = categoryData.keys.first;
 
   void _scrollToCategory(String category) {
     final keyContext = _categoryKeys[category]?.currentContext;
