a65e37420fa1a6b2569cbeb1960c3c552e9d262e
QForm <mr.bogdan2003@gmail.com>
2024-07-03 14:04:18 +0600

    refactor: split menu-screen file

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_coffee_menu/src/app.dart';
-import 'package:flutter_coffee_menu/src/common/theme/theme.dart';
 
 void main() {
   runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MenuScreen(),
-      theme: appTheme,
-    );
-  }
 }
